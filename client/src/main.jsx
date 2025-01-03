9185d64f39d848b9d0200376bd51460e36ece8bf
Aryan Parab <aryanparab@Aryans-MacBook-Pro.local>
2025-01-02 20:43:56 -0800

    complete sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import {store,persistor} from './redux/store.js';
 import {Provider} from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/themeProvider.jsx';
 
 
 createRoot(document.getElementById('root')).render(
